a2a3d502453a11f6ff51793ca965de6b50b5792f
joel rivera <jorivera114@gmail.com>
2023-08-07 15:39:59 -0400

    15

--- app.js
+++ app.js
@@ -15,6 +15,12 @@ const app = express();
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db", () => {});
 const routes = require("./routes");
 
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
+
 app.use(express.json());
 app.use(cors());
 app.use(routes);
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,10 @@ const user = require("./user");
 const { errors } = require("../utils/errors");
 const { loginUser, createUser } = require("../controllers/user");
 
-router.use("/items", clothingItem);
-router.use("/users", user);
 router.post("/signin", loginUser);
 router.post("/signup", createUser);
+router.use("/items", clothingItem);
+router.use("/users", user);
 
 router.use((req, res) => {
   res.status(errors.NOT_FOUND).send({
