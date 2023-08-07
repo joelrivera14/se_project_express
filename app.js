70d89488dbb7eb34c8dc7cb7243a3bcd217def26
joel rivera <jorivera114@gmail.com>
2023-08-07 14:00:27 -0400

    14

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const errorHandler = require("./middlewares/error-handler");
 const { errors } = require("celebrate");
+const errorHandler = require("./middlewares/error-handler");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
+require("dotenv").config();
+console.log(process.env.NODE_ENV);
 
 const { PORT = 3001 } = process.env;
+console.log({ PORT });
 const app = express();
 
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db", () => {});
